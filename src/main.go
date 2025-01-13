2a99650ea9831df1754cc92638140da3cf7c40d5
Komi Alasse <komialasse@gmail.com>
2025-01-12 23:08:00 -0800

    add flag

--- src/main.go
+++ src/main.go
@@ -6,5 +6,16 @@ import (
 )
 
 func main() {
-	fmt.Println(flag.Args());
+	var host string
+	var to string
+	var help string
+	flag.StringVar(&host, "host", "localhost", "the host to expose")
+	flag.StringVar(&to, "to", "server", "address of the remote server")
+	flag.String("help", "", "print help information")
+	flag.Parse()
+
+	
+	
+	fmt.Println(host, to, help)
+	fmt.Println(flag.Args())
 }
