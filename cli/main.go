b9cc8427b1c891e83ac54566218ee587443526d2
Komi Alasse <komialasse@gmail.com>
2025-01-25 21:46:52 -0800

    add sub command flag parsing

--- cli/main.go
+++ cli/main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/gopher"
 )
 
+const (
+	LOCAL = "local"
+	SERVER = "server"
+)
+
 func main() {
-	var host string
-	var to string
-	flag.StringVar(&host, "host", "localhost", "the host to expose")
-	flag.StringVar(&to, "to", "server", "address of the remote server")
-	flag.Parse()
+	command := os.Args[1]
 
+	switch command {
+	case LOCAL:
+		localPort, err := strconv.Atoi(os.Args[2])
+		subArgs := os.Args[3:]
+		if err != nil {
+			log.Fatalf("Could not parse local port %v", os.Args[2])
+		}
 
-	server := gopher.Server{}
-	server.Listen()
+		localfs := flag.NewFlagSet(LOCAL, flag.ExitOnError)
+		localhost := localfs.String("localhost", "localhost", "the local host to export")
+		to := localfs.String("to", "me", "address of remote server")
+		remote_port := localfs.Int("port", 8080, "port of remote server")
+
+		if err := localfs.Parse(subArgs); err != nil {
+			log.Fatal(err)
+		}
+		gopher.NewClient(*localhost, *to, localPort, *remote_port)
+		// msg := gopher.HelloMessage {}
+		// client.Send(msg)
+	case SERVER:
+		server := gopher.NewServer()
+		server.Listen()
+	default:
+		log.Fatal("unrecognized command")
+	}
 }
